3e645e34e846e70a088832455d18a5baac00f645
Curry W Hilton <cwhilton@students.cba.ua.edu>
2016-09-06 18:24:48 -0500

    clean up

--- arl0_ku_sp.R
+++
@@ -1,8 +0,0 @@
-ARL0 <- function (L,m,n) {
-  CARL <- function (U) {
-    a <- 1/(2*pnorm(-1*(L*sqrt(qchisq(U,m*(n-1))/(m*(n-1)))),0,1))
-    return(a)
-  }
-  a <- integrate(CARL,0,1)$va
-  return (a)
-}
