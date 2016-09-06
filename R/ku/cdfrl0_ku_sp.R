ea222f8d23777fc883408836283c1391c645dcbe
rjfaculty <rjfaculty@cba.ua.edu>
2016-09-06 18:27:01 -0500

    1

---
+++ R/ku/cdfrl0_ku_sp.R
@@ -0,0 +1,8 @@
+CDFRL0 <- function (t,m,n,L) {
+  inside <- function (U) {
+    a <- 1-((2*pnorm(1*(L*sqrt(qchisq(U,m*(n-1))/(m*(n-1)))),0,1)-1)^(t-1))*dunif(U,0,1)
+    return(a)
+  }
+  b <- integrate(inside,0,1)$va
+  return (b)
+}
