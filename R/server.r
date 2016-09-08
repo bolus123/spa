9c6e9af51b496458c5d34d9b5cc8435df056afc1
curryhilton <curry.hilton@gmail.com>
2016-09-08 06:35:04 -0500

    server update

--- R/server.r
+++ R/server.r
@@ -7,10 +7,21 @@ shinyServer(function(input, output) {
     1/(2-(2*pnorm(L)))
   })
   
+  q10 <- reactive({
+    L <- as.numeric(input$l)
+    qgeom(0.1, 2-(2*pnorm(L)))
+  })
+  
+  cdfrl0 <- reactive({
+    L <- as.numeric(input$l)
+    t <- 370
+    pgeom(t,2-(2*pnorm(L)))
+  })
+  
   tb <- reactive({
     data.frame(
-      Metric = c("ARL0"),
-      Value = as.numeric(c(arl0())),
+      Metric = c("ARL0", "Percentile: 10%", "CDFRL0"),
+      Value = as.numeric(c(arl0(), q10(), cdfrl0())),
       stringsAsFactors = FALSE
     )
   })
