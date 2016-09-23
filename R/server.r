56ff8dba7b293c93a859223a3661a8870011c8ed
curryhilton <curry.hilton@gmail.com>
2016-09-23 10:06:52 -0500

    server updates

--- R/server.r
+++ R/server.r
@@ -76,14 +76,56 @@ shinyServer(function(input, output) {
   
   output$cdf <- renderPlot({
     L <- as.numeric(input$l)
+    
     cdfrlc <- function(t, L){
       cdfrl0 <- pgeom(t, 2 - (2 * pnorm(L)))
       Vectorize(cdfrl0)
     }
-    cdf <- curve(cdfrlc(x, L), 1, 2000, n = 2000, ylim = c(0,1),
+    
+    arl0 <- function (L) {
+      a <- 1/(2-(2*pnorm(L)))
+      return(a)
+    }
+    
+    cdfrl0 <- function (t,L) {
+      b <- t - 1
+      a <- pgeom(b, 2-(2*pnorm(L)))
+      return(a)
+    }
+    
+    quantileRL0 <- function (p,L) {
+      a<-log(1-p)/log(1-(2-(2*pnorm(L))))
+      g<-ceiling(a)
+      return(g)
+    }
+    
+    arl0r <- round(arl0(L), 2)
+    
+    cdfmeanr <- round(cdfrl0(arl0(L), L), 2)
+    
+    median0 <- quantileRL0(0.5,L)
+    
+    cdfmedianr <-round(cdfrl0(quantileRL0(0.5,L),L),2)
+    
+    cdf <- curve(cdfrlc(x, L), 1, 2000, n = 10000, ylim = c(0,1),
               xlab = "t", ylab = "", cex.axis = 1.5, type = "l",
-              lty = 1, lwd = 3, yaxs="i", xaxs="i", 
+              lty = 1, lwd = 3, yaxs="i", xaxs="i", xaxt="n", yaxt="n",
               main = paste("P(In-Control RL <= t)","for", "L =", L))
+              xvalues<-c(0,200,400,600,800,1000,1200,1400,1600,1800,2000)
+              yvalues<-c(0,0.2,0.4,0.6,0.8,1)
+              axis(1,at=xvalues,cex.axis=1.5,las=1)
+              axis(2,at=yvalues,cex.axis=1.5,las=1)
+              axis(3,arl0r,cex.axis=1,las=1)
+              axis(4,cdfmeanr,cex.axis=1,las=1)
+              abline(v=arl0(L),lty=5.5,col="blue")
+              abline(h=cdfrl0(arl0(L),L),lty=5.5,col="blue")
+              axis(1,median0,cex.axis=1,las=1, line=1)
+              axis(4,cdfmedianr,cex.axis=1,las=1)
+              abline(v=median0 ,lty=5.5,col="red")
+              abline(h=cdfrl0(quantileRL0(0.5,L),L),lty=5.5,col="red")
+              legend(1250, 0.4, c( paste("MRL0 =", median0) , 
+                  paste("ARL0 =", arl0r)), cex=1, lty=c(5.5,5.5),lwd=c(1,1),
+                  col=c("red","blue"))
     cdf
   })
   
