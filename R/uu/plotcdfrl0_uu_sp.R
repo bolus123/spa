9daeac886858dbf783b4eb867d4c554f8f09f740
¨FelipeSJardim¨ <felipe_s_jardim@hotmail.com>
2016-10-09 05:52:14 -0300

    done

--- R/uu/plotcdfrl0_uu_sp.R
+++
@@ -1,6 +0,0 @@
-plotCDFRL0 <- function (m,n,L) {
-  CDFRL02 <- Vectorize(CDFRL0)
-  curve(CDFRL02(x,m,n,L),1,2000 ,ylim=c(0,1),xlab="t",ylab="",cex.axis=1.5,type="l",lty=1,lwd=3,yaxs="i",xaxs="i",main=paste("P(In-Control RL <= t)","for", "m=", m, "n=",n, "L=",L ))
-}
-
-####
